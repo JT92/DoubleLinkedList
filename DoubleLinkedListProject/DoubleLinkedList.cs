3443b5cc5545a4ea0799d4e83b03c83814935956
jack torres <jacintotorres92@outlook.com>
2016-09-28 00:22:08 -0400

    Added delete functions Added reverse function Tested

--- DoubleLinkedListProject/DoubleLinkedList.cs
+++ DoubleLinkedListProject/DoubleLinkedList.cs
@@ -22,6 +22,7 @@ namespace DoubleLinkedListProject
             if (start == null)
             {
                 Console.WriteLine("List is Empty");
+                return;
             }
 
             // If list contains data, traverse and print
@@ -35,14 +36,221 @@ namespace DoubleLinkedListProject
             Console.WriteLine();
         }
 
-        // Insert data into the beginning of the list
+        // Insert data into the beginning of the list (or into empty list)
         public void InsertInBeginning(int theValue)
         {
             Node temp = new Node(theValue);
+            
+            // if list empty
+            if (start == null)
+            {
+                start = temp;
+                return;
+            }
+
             temp.next = start;
             start.prev = temp;
             start = temp;
         }
+        
+        // Insert data into end of list (or into empty list)
+        public void InsertAtEnd(int theValue)
+        {
+            Node temp = new Node(theValue);
+
+            // if list is empty
+            if (start == null)
+            {
+                start = temp;
+                return;
+            }
+
+            Node p = start;
+            while (p.next != null)
+                p = p.next;
+            p.next = temp;
+            temp.prev = p;
+        }
+
+        // Find node by value
+        public Node FindNode(int theValue)
+        {
+            // find p (node)
+            Node p = start;
+            while (p != null)
+            {
+                if (p.value == theValue)
+                    break;
+                p = p.next;
+            }
+            return p;
+        }
+
+        // Insert data after a node
+        public void InsertAfterNode(int theNode, int theValue)
+        {
+            // find p (node)
+            Node p = FindNode(theNode);
+
+            if (p == null)
+                Console.WriteLine(theNode + " could not be found");
+            else
+            {
+                // insert new node
+                Node temp = new Node(theValue);
+                temp.prev = p;
+                temp.next = p.next;
+                if (p.next != null)
+                    p.next.prev = temp; // should not be done if next node is null
+                p.next = temp;
+            }
+
+        }
+
+        // Insert data before a node
+        public void InsertBeforeNode(int theNode, int theValue)
+        {
+            // find p (node)
+            Node p = FindNode(theNode);
+
+            // if list is empty
+            if (p == null)
+                Console.WriteLine(theNode + " could not be found");
+            
+            // if you are trying to insert before first node
+            else if (start.value == theNode)
+            {
+                InsertInBeginning(theValue);
+                return;
+            }
+            else
+            {
+                // insert new node
+                Node temp = new Node(theValue);
+                temp.prev = p.prev;
+                temp.next = p;
+                p.prev.next = temp;
+                p.prev = temp;
+            }
+        }
+
+        // Delete the first Node
+        public void DeleteFirstNode()
+        {
+            // if no nodes in list
+            if (start == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            
+            // if one node in list
+            if (start.next == null)
+                start = null;
+
+            // if more than one nodes in list
+            else
+            {
+                start = start.next;
+                start.prev = null;
+            }
+        }
+
+        // Delete the last node in the list
+        public void DeleteLastNode()
+        {
+            // if no nodes in list
+            if (start == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+
+            // if one node in list
+            if (start.next == null)
+                start = null;
+
+            // if more than one nodes in list
+            else
+            {
+                Node p = start;
+                while (p.next.next != null)
+                    p = p.next;
+                p.next = null;
+            }
+        }
+
+        // Delete specified Node
+        public void DeleteSpecifiedNode(int theNode)
+        {
+            // if no nodes in list
+            if (start == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+
+            // find the Node
+            Node p = FindNode(theNode);
+
+            // if at beginning
+            if (p.prev == null)
+                DeleteFirstNode();
+
+            // if at end
+            else if (p.next == null)
+                DeleteLastNode();
+
+            // if in between nodes
+            else
+            {
+                p.prev.next = p.next;
+                p.next.prev = p.prev;
+            }
+        }
+        
+        // Reverse the list
+        public void ReverseList()
+        {
+            // if there are 0 or 1 elements, do nothing
+            if (start == null || start.next == null)
+                return;
+
+            Node p1 = start;                // set p1 to first node
+            Node p2 = start.next;           // set p2 to second node
+
+            while (p2 != null)
+            {
+                p1.next = p1.prev;          
+                p1.prev = p2;
+                p1 = p2;
+                p2 = p2.next;
+            }
+            p1.next = p1.prev;
+            p1.prev = null;
+            start = p1;
+        }
+
+
+        // Create a list
+        public void CreateList()
+        {
+            int i, n, theValue;
+            Console.Write("Enter the number of nodes to insert: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+                return;
+
+            for ( i = 1; i<=n; i++)
+            {
+                Console.Write("Enter an element to be inserted: ");
+                theValue = Convert.ToInt32(Console.ReadLine());
+                InsertAtEnd(theValue);
+            }
+            
+        }
+
 
     }
 }
--- DoubleLinkedListProject/Program.cs
+++ DoubleLinkedListProject/Program.cs
@@ -12,34 +12,87 @@ namespace DoubleLinkedListProject
         {
             // Create the list
             DoubleLinkedList list = new DoubleLinkedList();
+            list.CreateList();
             int selection = 0;
 
             // Display the Menu
             do
             {
+                Console.WriteLine();
                 Console.WriteLine("[1] Print out the list");
-                Console.WriteLine("[2] Insert a node in beginning of the list");
-                Console.WriteLine("[7] Exit the Application");
-                selection = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("[2] Insert a node at the beginning of the list");
+                Console.WriteLine("[3] Insert a node at the end of the list");
+                Console.WriteLine("[4] Insert after a specified node");
+                Console.WriteLine("[5] Insert before a specified node");
+                Console.WriteLine("[6] Delete first node");
+                Console.WriteLine("[7] Delete last node");
+                Console.WriteLine("[8] Delete a specified node");
+                Console.WriteLine("[9] Reverse the list");
+                Console.WriteLine("[10] Exit the Application");
 
-                switch( selection )
+                try
                 {
-                    case 1:
-                        list.PrintList();
-                        break;
-                    case 2:
-                        list.InsertInBeginning();
-                        break;
-                    case 7:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Please Select a choice from the menu");
-                        break;
+                    selection = Convert.ToInt32(Console.ReadLine());
+                    switch( selection )
+                    {
+                        case 1:
+                            list.PrintList();
+                            break;
+                        case 2:
+                            Console.Write("Enter the value you want to insert: ");
+                            list.InsertInBeginning(ReadInt());
+                            break;
+                        case 3:
+                            Console.Write("Enter the value you want to insert: ");
+                            list.InsertAtEnd(ReadInt());
+                            break;
+                        case 4:
+                            {
+                                Console.Write("Enter the value you want to insert: ");
+                                int insertValue = ReadInt();
+                                Console.Write("Enter the element after which to insert: ");
+                                list.InsertAfterNode(ReadInt(), insertValue);
+                                break;
+                            }
+                        case 5:
+                            {
+                                Console.Write("Enter the value you want to insert: ");
+                                int insertValue = ReadInt();
+                                Console.Write("Enter the element before which to insert: ");
+                                list.InsertBeforeNode(ReadInt(), insertValue);
+                                break;
+                            }
+                        case 6:
+                            list.DeleteFirstNode();
+                            break;
+                        case 7:
+                            list.DeleteLastNode();
+                            break;
+                        case 8:
+                            Console.Write("Enter the value of the node you want to delete: ");
+                            list.DeleteSpecifiedNode(ReadInt());
+                            break;
+                        case 9:
+                            list.ReverseList();
+                            break;
+                        case 10:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Please Select a choice from the menu");
+                            break;
+                    }
                 }
+                catch
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+            } while (selection != 10);
+        }
 
-            } while (selection != 7);
-
+        public static int ReadInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
         }
     }
 }
